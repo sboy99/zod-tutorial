490133a73ff7bdd5cc7ed883cfda353e8654edd2
Sagar Bera <contact.sagarber@gmail.com>
2023-08-16 09:13:43 +0530

    chore: solved e-10

--- src/10-transform.problem.ts
+++ src/10-transform.problem.ts
@@ -3,30 +3,31 @@
 import { expect, it } from "vitest";
 import { z } from "zod";
 
-const StarWarsPerson = z.object({
-  name: z.string(),
-});
+const StarWarsPerson = z
+    .object({
+        name: z.string(),
+    })
+    .transform(({ name }) => ({ name, nameAsArray: name.split(" ") }));
 //^ 🕵️‍♂️
 
 const StarWarsPeopleResults = z.object({
-  results: z.array(StarWarsPerson),
+    results: z.array(StarWarsPerson),
 });
 
 export const fetchStarWarsPeople = async () => {
-  const data = await fetch(
-    "https://www.totaltypescript.com/swapi/people.json",
-  ).then((res) => res.json());
+    const data = await fetch(
+        "https://www.totaltypescript.com/swapi/people.json"
+    ).then((res) => res.json());
 
-  const parsedData = StarWarsPeopleResults.parse(data);
-
-  return parsedData.results;
+    const parsedData = StarWarsPeopleResults.parse(data);
+    return parsedData.results;
 };
 
 // TESTS
 
 it("Should resolve the name and nameAsArray", async () => {
-  expect((await fetchStarWarsPeople())[0]).toEqual({
-    name: "Luke Skywalker",
-    nameAsArray: ["Luke", "Skywalker"],
-  });
+    expect((await fetchStarWarsPeople())[0]).toEqual({
+        name: "Luke Skywalker",
+        nameAsArray: ["Luke", "Skywalker"],
+    });
 });
