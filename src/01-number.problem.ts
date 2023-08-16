537c869b8e54224c54e0de77ac07192fe78d2b52
Sagar Bera <contact.sagarber@gmail.com>
2023-08-16 08:43:03 +0530

    chore: solved e-01

--- src/01-number.problem.ts
+++ src/01-number.problem.ts
@@ -3,19 +3,20 @@
 import { expect, it } from "vitest";
 import { z } from "zod";
 //       ^ 🕵️‍♂️
-
+const isNumber = z.number();
 export const toString = (num: unknown) => {
-  return String(num);
+    const parsed = isNumber.parse(num);
+    return String(parsed);
 };
 
 // TESTS
 
 it("Should throw a runtime error when called with not a number", () => {
-  expect(() => toString("123")).toThrowError(
-    "Expected number, received string",
-  );
+    expect(() => toString("123")).toThrowError(
+        "Expected number, received string"
+    );
 });
 
 it("Should return a string when called with a number", () => {
-  expect(toString(1)).toBeTypeOf("string");
+    expect(toString(1)).toBeTypeOf("string");
 });
