96001534e41a6c18027ee9e7949aa39099cde64a
Sagar Bera <contact.sagarber@gmail.com>
2023-08-16 08:51:56 +0530

    chore: solved e-05

--- src/05-optional.problem.ts
+++ src/05-optional.problem.ts
@@ -4,34 +4,34 @@ import { expect, it } from "vitest";
 import { z } from "zod";
 
 const Form = z.object({
-  name: z.string(),
-  phoneNumber: z.string(),
-  //                     ^ 🕵️‍♂️
+    name: z.string(),
+    phoneNumber: z.string().optional(),
+    //                     ^ 🕵️‍♂️
 });
 
 export const validateFormInput = (values: unknown) => {
-  const parsedData = Form.parse(values);
+    const parsedData = Form.parse(values);
 
-  return parsedData;
+    return parsedData;
 };
 
 // TESTS
 
 it("Should validate correct inputs", async () => {
-  expect(() =>
-    validateFormInput({
-      name: "Matt",
-    }),
-  ).not.toThrow();
+    expect(() =>
+        validateFormInput({
+            name: "Matt",
+        })
+    ).not.toThrow();
 
-  expect(() =>
-    validateFormInput({
-      name: "Matt",
-      phoneNumber: "123",
-    }),
-  ).not.toThrow();
+    expect(() =>
+        validateFormInput({
+            name: "Matt",
+            phoneNumber: "123",
+        })
+    ).not.toThrow();
 });
 
 it("Should throw when you do not include the name", async () => {
-  expect(() => validateFormInput({})).toThrowError("Required");
+    expect(() => validateFormInput({})).toThrowError("Required");
 });
