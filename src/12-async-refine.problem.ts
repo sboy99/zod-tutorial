b1447546d79904ea5ab2454b902d7f84ae820e92
Sagar Bera <contact.sagarber@gmail.com>
2023-08-16 09:22:23 +0530

    chore: solved e-12

--- src/12-async-refine.problem.ts
+++ src/12-async-refine.problem.ts
@@ -5,42 +5,45 @@ import { z } from "zod";
 
 // HINT - use me!
 const doesStarWarsPersonExist = async (id: string) => {
-  try {
-    const data = await fetch(
-      "https://www.totaltypescript.com/swapi/people/" + id + ".json",
-    ).then((res) => res.json());
-    return Boolean(data?.name);
-  } catch (e) {
-    // Returns false if not found
-    return false;
-  }
+    try {
+        const data = await fetch(
+            "https://www.totaltypescript.com/swapi/people/" + id + ".json"
+        ).then((res) => res.json());
+        return Boolean(data?.name);
+    } catch (e) {
+        // Returns false if not found
+        return false;
+    }
 };
 
 const Form = z.object({
-  id: z.string(),
-  //           ^ 🕵️‍♂️
+    id: z.string().refine(async (id) => {
+        if (!(await doesStarWarsPersonExist(id))) throw new Error("Not found");
+        return id;
+    }),
+    //           ^ 🕵️‍♂️
 });
 
 export const validateFormInput = async (values: unknown) => {
-  const parsedData = await Form.parseAsync(values);
+    const parsedData = await Form.parseAsync(values);
 
-  return parsedData;
+    return parsedData;
 };
 
 // TESTS
 
 it("Should fail if the star wars person does not exist", async () => {
-  await expect(
-    validateFormInput({
-      id: "123123123123123123",
-    }),
-  ).rejects.toThrow("Not found");
+    await expect(
+        validateFormInput({
+            id: "123123123123123123",
+        })
+    ).rejects.toThrow("Not found");
 });
 
 it("Should succeed if the star wars person does exist", async () => {
-  expect(
-    await validateFormInput({
-      id: "1",
-    }),
-  ).toEqual({ id: "1" });
+    expect(
+        await validateFormInput({
+            id: "1",
+        })
+    ).toEqual({ id: "1" });
 });
