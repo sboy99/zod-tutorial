10ac94ef7f15d33d2cd201b9bf31180556bea3a7
Sagar Bera <contact.sagarber@gmail.com>
2023-08-16 08:48:44 +0530

    chore: solved e-03

--- src/03-array.problem.ts
+++ src/03-array.problem.ts
@@ -4,26 +4,26 @@ import { expect, it } from "vitest";
 import { z } from "zod";
 
 const StarWarsPerson = z.object({
-  name: z.string(),
+    name: z.string(),
 });
 
-const StarWarsPeopleResults = z.unknown();
+const StarWarsPeopleResults = z.object({
+    results: z.array(StarWarsPerson),
+});
 //                            ^ 🕵️‍♂️
 
 export const fetchStarWarsPeople = async () => {
-  const data = await fetch(
-    "https://www.totaltypescript.com/swapi/people.json",
-  ).then((res) => res.json());
-
-  const parsedData = StarWarsPeopleResults.parse(data);
-
-  return parsedData.results;
+    const data = await fetch(
+        "https://www.totaltypescript.com/swapi/people.json"
+    ).then((res) => res.json());
+    const parsedData = StarWarsPeopleResults.parse(data);
+    return parsedData.results;
 };
 
 // TESTS
 
 it("Should return the name", async () => {
-  expect((await fetchStarWarsPeople())[0]).toEqual({
-    name: "Luke Skywalker",
-  });
+    expect((await fetchStarWarsPeople())[0]).toEqual({
+        name: "Luke Skywalker",
+    });
 });
